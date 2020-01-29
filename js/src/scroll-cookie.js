9082d16bb1905fd95bf3a66c1d6047a37f9411dc
sushan102 <3295469681@qq.com>
2020-01-29 14:19:55 +0800

    Site updated: 2020-01-29 14:19:32

--- js/src/scroll-cookie.js
+++ js/src/scroll-cookie.js
@@ -1,3 +1,3 @@
-// build time:Tue Jan 28 2020 15:40:06 GMT+0800 (GMT+08:00)
+// build time:Wed Jan 29 2020 11:29:20 GMT+0800 (GMT+08:00)
 $(document).ready(function(){var o,i=window.location.href.replace(window.location.origin,"");if($(window).on("scroll",function(){clearTimeout(o),o=setTimeout(function(){Cookies.set("scroll-cookie",$(window).scrollTop()+"|"+i,{expires:365,path:""})},250)}),void 0!==Cookies.get("scroll-cookie")){var e=Cookies.get("scroll-cookie").split("|");e[1]===i&&$(window).scrollTop(e[0])}});
 //rebuild by neat 
