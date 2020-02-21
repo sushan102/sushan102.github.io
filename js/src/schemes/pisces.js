f1a9a20fe0108638d40f9de550ec00d2daf07950
sushan102 <3295469681@qq.com>
2020-02-21 18:09:29 +0800

    Site updated: 2020-02-21 18:09:16

--- js/src/schemes/pisces.js
+++ js/src/schemes/pisces.js
@@ -1,3 +1,3 @@
-// build time:Fri Feb 21 2020 17:43:46 GMT+0800 (GMT+08:00)
+// build time:Fri Feb 21 2020 18:08:26 GMT+0800 (GMT+08:00)
 $(document).ready(function(){var e=$(".sidebar-inner"),i=CONFIG.sidebar.offset?CONFIG.sidebar.offset:12;function t(){var t,a,f=$(".header-inner").height()+i,o=(t=$(".footer-inner"),a=t.outerHeight(!0)-t.outerHeight(),t.outerHeight(!0)+a);f+($("#sidebar").height()+NexT.utils.getSidebarb2tHeight())<$("#content").height()&&(e.affix({offset:{top:f-i,bottom:o}}),e.affix("checkPosition")),$("#sidebar").css({"margin-top":f,"margin-left":"initial"})}t(),window.matchMedia("(min-width: 992px)").addListener(function(i){i.matches&&($(window).off(".affix"),e.removeData("bs.affix").removeClass("affix affix-top affix-bottom"),t())})});
 //rebuild by neat 
